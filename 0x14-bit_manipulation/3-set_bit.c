be93b8dc39e014b6ee939ae1a2960967aee4d001
MounaSaiss <mouna1.saiss@gmail.com>
2023-12-14 13:55:13 -0800

    first commit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,30 +1,18 @@
-#include <stdio.h>
+#include "main.h"
 
-int set_bit(unsigned long int *n, unsigned int index) {
-    // Check if the index is valid
-    if (index >= sizeof(unsigned long int) * 8) {
-        return -1;  // Error: Index out of range
-    }
+/**
+ * set_bit - sets the value of a bit to 1 at a given index.
+ * @n: the number
+ * @index: the index
+ * Return: an integer
+ */
 
-    // Set the bit at the given index to 1
-    *n |= (1UL << index);
-
-    return 1;  // Success
-}
-
-int main() {
-    unsigned long int num = 42;  // Example number
-    unsigned int index_to_set = 3;  // Example index
-
-    // Call the set_bit function
-    int result = set_bit(&num, index_to_set);
-
-    // Check the result
-    if (result == 1) {
-        printf("Bit at index %u set to 1. New value: %lu\n", index_to_set, num);
-    } else {
-        printf("Error: Index out of range\n");
-    }
-
-    return 0;
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(size_t) * 8)
+		return (-1);
+	if (*n & (1 << index))
+		return (1);
+	*n = *n + (1 << index);
+	return (1);
 }
